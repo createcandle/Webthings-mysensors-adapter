255329eafc226576a8a843f0f9476ae94272f89c
Create Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2019-08-24 00:41:35 +0200

    0.1.4

--- package.sh
+++ package.sh
@@ -4,20 +4,30 @@ set -e
 
 version=$(grep version package.json | cut -d: -f2 | cut -d\" -f2)
 
+
 # Clean up from previous releases
 rm -rf *.tgz package
 rm -f SHA256SUMS
+rm -rf lib
+
 
 # Put package together
 mkdir package
-cp -r pkg LICENSE package.json *.py requirements.txt setup.cfg package/
+mkdir lib
+
+# Pull down Python dependencies
+pip3 install -r requirements.txt -t lib --no-binary pymysensors,paho-mqtt --prefix ""
+
+cp -r pkg lib LICENSE package.json *.py package/
 find package -type f -name '*.pyc' -delete
 find package -type d -empty -delete
 
 # Generate checksums
 cd package
-sha256sum *.py pkg/*.py LICENSE requirements.txt setup.cfg > SHA256SUMS
+sha256sum *.py pkg/*.py LICENSE > SHA256SUMS
 cd -
 
 # Make the tarball
 tar czf "mysensors-adapter-${version}.tgz" package
+sha256sum "mysensors-adapter-${version}.tgz"
+#sudo systemctl restart mozilla-iot-gateway.service
