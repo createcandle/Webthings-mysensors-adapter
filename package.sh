a43d63cc5ef699c935bd012faaeec9b16031ac6b
Michael Stegeman <michael@stegeman.me>
2020-06-16 22:37:23 -0800

    Prep package for addon-builder.
    References #9

--- package.sh
+++ package.sh
@@ -1,38 +1,37 @@
-#!/bin/bash
-
-set -e
+#!/bin/bash -e
 
 version=$(grep version package.json | cut -d: -f2 | cut -d\" -f2)
 
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  PYTHON_VERSION="$(python3 --version 2>&1 | cut -d' ' -f2 | cut -d. -f 1-2)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
+fi
 
 # Clean up from previous releases
-rm -rf *.tgz package lib
-rm -f SHA256SUMS
-rm -rf lib
-rm -rf ._*
+rm -rf *.tgz package SHA256SUMS lib
 
-# Put package together
-mkdir package
-mkdir lib
+# Prep new package
+mkdir lib package
 
 # Pull down Python dependencies
-pip3 install -r requirements.txt -t lib --no-binary pymysensors,paho-mqtt --prefix ""
+pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
 
-#temporary fix
-#cp VERSION ./lib/mysensors/
-
-cp -r pkg lib LICENSE package.json manifest.json *.py setup.cfg requirements.txt package/
+# Put package together
+cp -r lib pkg LICENSE manifest.json package.json *.py README.md package/
 find package -type f -name '*.pyc' -delete
-#find package -type d -empty -delete
+find package -type d -empty -delete
 
 # Generate checksums
 cd package
-#sha256sum *.py pkg/*.py LICENSE > SHA256SUMS
-find . -type f \! -name SHA256SUMS -exec sha256sum {} \; >> SHA256SUMS
+find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd -
 
 # Make the tarball
-tar czf "mysensors-adapter-${version}.tgz" package
-sha256sum "mysensors-adapter-${version}.tgz"
-#sudo systemctl restart mozilla-iot-gateway.service
+TARFILE="mysensors-adapter-${version}${TARFILE_SUFFIX}.tgz"
+tar czf ${TARFILE} package
 
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
+rm -rf SHA256SUMS package
